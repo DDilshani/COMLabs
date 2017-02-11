9bf9469e94c7c5a4f7b0cacf11c3f9e8312df1d5
udareechk <udareeck@gmail.com>
2017-02-11 23:54:05 +0530

    Initial Commit

---
+++ SEMESTER5/CO322/Lab03/regEx.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.*;
+
+public class regEx {
+
+
+	public static void main(String[] args) throws Exception {
+
+		String pathToFile = args[0];
+		String letters = args[1];
+
+		char [] charLetters = letters.toCharArray();
+
+		BufferedReader br = new BufferedReader(new FileReader(pathToFile));
+
+		String line = br.readLine();
+
+		while (line != null) {
+
+			// System.out.println(line);
+			char [] word = line.toCharArray();
+			int count = 0;
+
+			for (char c : word ) {
+
+				for (char s : charLetters ) {
+
+					if ( c == s) {
+
+						count++;
+						
+					}	
+				}
+
+				if (count == 0) {
+					
+					break;
+				}
+
+			}
+			
+
+			if ((count == line.length()) && (line != " ")){
+				
+				System.out.println(line);
+
+			}
+
+	
+			line = br.readLine();
+			
+		}
+		
+	}
+}

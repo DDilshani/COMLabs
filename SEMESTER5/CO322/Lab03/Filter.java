8dd07b03256d23a9e18b564180b444f8c2b711fa
udareechk <udareeck@gmail.com>
2017-02-21 18:43:54 +0530

    Lab03: Filter modified

---
+++ SEMESTER5/CO322/Lab03/Filter.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.*;
+
+public class Filter {
+
+
+	public static void main(String[] args) throws Exception {
+
+		String pathToFile = args[0];
+		String letters = args[1];
+
+		char [] charLetters = letters.toCharArray();
+
+		int [] sampleLetterSum = new int [26];
+
+		for (char c : charLetters ) {
+
+			int index = c - 'a';
+			sampleLetterSum[index] = sampleLetterSum[index] + 1;
+			
+		}
+
+		int [] wordLetterSum = new int [26];
+
+		BufferedReader br = new BufferedReader(new FileReader(pathToFile));
+
+		String line;
+
+		while ((line = br.readLine()) != null) {
+
+			// System.out.println(line);
+			char [] word = line.toCharArray();
+			boolean match = true;
+
+			for (char c : word ) {
+
+				if((c >= 'a') && (c <= 'z') ){
+
+					int index = c - 'a';
+					// System.out.println(index);
+					wordLetterSum[index] = wordLetterSum[index] + 1;
+
+					if(wordLetterSum[index] > sampleLetterSum[index]){
+
+						match = false;
+						break;
+					}
+
+				} else {
+
+					match = false;
+					break;
+				}
+
+			}
+
+			if (match){
+
+				System.out.println(line);
+
+			}
+			
+
+	
+			// line = br.readLine();
+			
+		}
+		
+	}
+
+}

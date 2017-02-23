d8b3539e38599c773321bd8b86c4382f2e1a032f
udareechk <udareeck@gmail.com>
2017-02-23 15:21:33 +0530

    Initial Commit

---
+++ SEMESTER5/CO324/Lab05/Address_Book/src/java/AddressBook.java
@@ -0,0 +1,114 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.*;
+import java.io.*;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nathasha
+ */
+
+//Logic for your address book
+public class AddressBook {
+    
+    // instance address book
+    public HashMap <String, List <String>> contactsMap;
+    private final String pathToCSV;
+    
+    //read from file and create Address Book
+    public AddressBook(String fileName) throws Exception{
+    
+        /*TODO*/
+        //Create your address book
+        contactsMap = new HashMap <String, List <String>>();
+        this.pathToCSV = fileName;
+        
+        //Read file
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+        String line = br.readLine();
+
+        // while ( (br.readLine()) != null){
+        while ( line != null){
+
+            String[] array = line.split(",");
+
+            String data [] = Arrays.copyOfRange(array, 1, array.length);
+        
+            // Storing contacts in an array list
+            List <String> contacts = new ArrayList <>(Arrays.asList(data));
+
+            contactsMap.put(array[0], contacts);
+
+            line = br.readLine();
+
+        }    
+    }
+    
+//    public AddressBook() throws Exception{
+//    
+//        /*TODO*/
+//        //Create your address book
+//        contactsMap = new HashMap <String, List <String>>();
+//    }
+    
+    
+    //search details of the requested contact in the address book
+    public String search(String name){
+	
+        /*TODO*/
+        if (contactsMap.containsKey(name)) {
+
+            return contactsMap.get(name).toString();
+
+        } else {
+
+            return ("Contacts for name "+ name +" is not found!");
+
+        }
+    }
+    
+    public synchronized void add(String name, String details){
+        
+        String [] contacts = details.split(",");
+        
+        List <String> data = new ArrayList <>(Arrays.asList(contacts));
+        
+        contactsMap.put(name, data);
+        
+    }
+    
+//    public void writeToCSV() throws IOException{
+//        
+//       BufferedWriter bw = new BufferedWriter(new FileWriter(pathToCSV));
+//
+//    	for (String key : contactsMap.keySet() ) {
+//
+//    		List <String> temp = contactsMap.get(key);
+//    		temp.add(0, key);
+//
+//    		String content = String.join(",", temp);
+//    		// System.out.println(content);
+//
+//    		// bw.write(key);
+//    		bw.write(content);
+//    		bw.write("\n");
+//    		bw.flush();
+//    		
+//    	}
+//    }
+}
+
+
---
+++ SEMESTER5/CO324/Lab05/Address_Book/src/java/AddressBookServlet.java
@@ -0,0 +1,217 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Nathasha
+ */
+
+@WebServlet(
+        name = "Address Book",
+        urlPatterns = {"/search","/newEntry"},
+        initParams = {@WebInitParam(name="filename", value="/addressBook.txt")}
+)
+
+public class AddressBookServlet extends HttpServlet {
+       
+    String path;
+//    AddressBook addressBook;
+    
+    //Servlet initialization
+    @Override
+    public void init() throws ServletException {
+        
+        
+            ServletConfig config = getServletConfig();
+            
+            //get the value of the init-parameter
+            String filename = config.getInitParameter("filename");
+            ServletContext sc = config.getServletContext();
+            path = sc.getRealPath(filename);
+    }
+           
+//        try{    
+//            addressBook = new AddressBook(path);
+//        } catch (Exception ex) {
+//            Logger.getLogger(AddressBookServlet.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        
+      
+//    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+            
+    
+//    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        response.setContentType("text/html;charset=UTF-8");
+//        
+//        /*TODO*/
+//        //process request parameters and add details coresponding to name       
+//        String name = request.getParameter("name");
+//        String contacts = request.getParameter("contacts");
+//        
+//        addressBook.add(name, contacts);
+//               
+//        response.setStatus(SC_OK);
+//        PrintWriter out = response.getWriter();
+//        out.println("Your contact details added!<br>"); 
+//        
+//        String content = addressBook.search(name);
+//        out.println(name + ": "+ content);
+//
+//        
+//    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String name = request.getParameter("name");
+        
+        HttpSession session = request.getSession();
+        
+        AddressBook addressBook;
+        String id = "contacts";
+           
+        if(session.isNew()){
+        
+            try {
+                
+                addressBook = new AddressBook(path);
+                session.setAttribute(id, addressBook);
+                
+                
+            } catch (Exception ex) {
+                Logger.getLogger(AddressBookServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+        } 
+        
+        addressBook = (AddressBook) session.getAttribute(id);
+        
+        String details;
+
+//        HttpSession session = request.getSession(true);    
+
+        if ((details = addressBook.search(name)) != null) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            PrintWriter out = response.getWriter();
+            out.println(name +" : "+details);
+        } else
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String name = request.getParameter("name");
+        String contacts = request.getParameter("contacts");
+        
+        AddressBook addressBook;
+        String id = "details";
+        
+        HttpSession session = request.getSession();   
+        
+        if(session.isNew()){
+        
+            try {
+                addressBook = new AddressBook(path);
+                session.setAttribute(id, addressBook);
+                
+            } catch (Exception ex) {
+                Logger.getLogger(AddressBookServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+        }
+        
+        addressBook = (AddressBook)session.getAttribute(id);
+
+        addressBook.add(name, contacts);
+        
+        session.setAttribute(id, addressBook);
+
+        response.setStatus(SC_OK);
+        PrintWriter out = response.getWriter();
+        out.println("<p>Your contact details added!</p>"); 
+
+        String content = addressBook.search(name);
+        out.println("<p>"+ name + ": "+ content +"</p>");
+       
+        
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+    
+//    @Override
+//    public void destroy (){
+//        
+//        try {
+//            addressBook.writeToCSV();
+//        } catch (IOException ex) {
+//            Logger.getLogger(AddressBookServlet.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+
+}

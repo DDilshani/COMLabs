aa26a223591784a13ad3d52489fff9597bab785b
udareechk <udareeck@gmail.com>
2017-02-23 16:03:40 +0530

    Initial Commit

---
+++ SEMESTER5/CO322/Lab04/MyList.java
@@ -0,0 +1,38 @@
+
+
+public class MyList <T> {
+
+	private Node head;
+	private Node tail = ; 
+
+	public MyList(Node node){
+
+		this.data = data;
+
+	}
+}
+
+public class Node <T> {
+
+	private T data;
+	private int pointer;
+
+	public Node(T data, int pointer){
+
+		this.data = data;
+		this.pointer = pointer;
+
+	}
+
+	public T getData(){
+
+		return this.data
+	}
+
+	public T getPointer(){
+
+		return this.pointer;
+	}
+
+
+}

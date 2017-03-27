05307ad7ca21c8184acf9db6b50f34e5284849a8
udareechk <udareeck@gmail.com>
2017-03-27 21:43:32 +0530

    Sparse Matrix finished

---
+++ SEMESTER5/CO322/Lab05/SparseMatrix.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author Nathasha Naranpanawa
+ */
+public class SparseMatrix {
+    
+    
+    public SparseMatrix(){}
+    
+    // create an empty square matrix of given size
+    public void createEmptyMatrix(int size){}
+
+    // put A[i][j] = 1  
+    public void set(int i, int j, int value){}
+    
+    // return value at A[i][j]
+    public int get(int i, int j){    
+        return -1;
+    }
+    
+}

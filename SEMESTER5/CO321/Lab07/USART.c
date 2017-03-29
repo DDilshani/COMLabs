adff57ca609a1b74a0e8b994691b0235626905fb
udareechk <udareeck@gmail.com>
2017-03-29 19:23:31 +0530

    USART modified

--- SEMESTER5/CO321/Lab07/USART.c
+++ SEMESTER5/CO321/Lab07/USART.c
@@ -1,5 +1,8 @@
 #include <avr/io.h>
 
+#define BAUD 9600
+#define MYUBRR FOSC/16/BAUD-1
+
 int main(){
 
 	usart_init();
@@ -23,4 +26,16 @@ int main(){
 			usart_send(data_snd[i]);
 		}
 	}
+}
+
+usart_init(){
+
+	//Set baud rate 
+	UBRR0H = (unsigned char)(ubrr>>8);
+	UBRR0L = (unsigned char)ubrr;
+	//Enable receiver and transmitter 
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	//Set frame format: 8data, 2stop bit 
+	UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+
 }

55adbc2d6093e3115cfc42e910f9925d2bb27658
udareechk <udareeck@gmail.com>
2017-04-20 14:56:20 +0530

    Serial Communication completed

--- SEMESTER5/CO321/Lab07/USART.c
+++ SEMESTER5/CO321/Lab07/USART.c
@@ -1,7 +1,11 @@
 #include <avr/io.h>
 
 #define BAUD 9600
-#define MYUBRR FOSC/16/BAUD-1
+#define MYUBRR 103
+
+char usart_receive();
+void usart_send(char data);
+void usart_init();
 
 int main(){
 
@@ -13,22 +17,28 @@ int main(){
 
 		char ch;
 		int i = 0;
-		int count;
+		int count = 256;
+
+		int j;
+		for (j = 0; j<256; j++){
+			data_snd[j] = 'a';
+		}
+		// data_snd[255] = '\r';
+
+		for (i = 0; i < count; i++){
+			usart_send(data_snd[i]);
+		}
 
 		while((ch = usart_receive()) != '\r'){
 			data_rec[i] = ch;
 			i++;
 		}
 
-		count = i;
-
-		for (i = 0; i < count; ++i){
-			usart_send(data_snd[i]);
-		}
 	}
 }
 
-usart_init(){
+
+void usart_init(){
 
 	//Set baud rate 
 	UBRR0H = (unsigned char)(MYUBRR>>8);
@@ -42,12 +52,23 @@ usart_init(){
 	UCSR0C |= (1<<UCSZ01);
 
 	//USCR0C --> UPMn1, UPMn0 = 0 --> set parity bits = 0
-}
 
-usart_send(){
 
 }
 
-usart_receive(){
-	
+char usart_receive(){
+	while (UCSR0A && (1 << RXC0) == 0) {
+	}
+
+	char data = UDR0;
+	return data;
+}
+
+void usart_send(char data){
+
+	while (UCSR0A && (1 << UDRE0) == 0) {
+	}
+
+	UDR0 = data;
+
 }

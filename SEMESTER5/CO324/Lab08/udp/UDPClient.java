19720a56b3bd1db2c13efe5897bbd0d5dc8a0458
udareechk <udareeck@gmail.com>
2017-04-21 18:07:12 +0530

    Modified

--- SEMESTER5/CO324/Lab08/udp/UDPClient.java
+++ SEMESTER5/CO324/Lab08/udp/UDPClient.java
@@ -2,41 +2,33 @@
 
 import java.net.*;
 import java.io.*;
-import java.lang.*;
 
 public class UDPClient {
 
     final static String data = "Hello there!";
-    final static int PORT = 1200;
+    final static int PORT = 1234;
     final static int BUFSIZE = 1024;
+    final static int PCKTS = 1000;
 
     public static void main(String args[]) {
 
         try ( DatagramSocket aSocket = new DatagramSocket();) {
             InetAddress aHost = InetAddress.getByName(args[0]);
             
-            // int count = 0;
-            // long receiveStart = 0;
-
-            for (int i=0; i<100 ; i++) {
 
                 byte[] dataArray = data.getBytes();
                 DatagramPacket requestPacket = new DatagramPacket(dataArray, dataArray.length, aHost, PORT);
+            
+            for (int i=0; i <= PCKTS ; i++) {
                 aSocket.send(requestPacket);
+                System.out.println("in");
 
-                byte[] buffer = new byte[BUFSIZE];
-                DatagramPacket recievePacket = new DatagramPacket(buffer, buffer.length);
-                // receiveStart = System.currentTimeMillis();
-                aSocket.receive(recievePacket);
-                // count++;
-                System.out.println("Reply:" + new String(recievePacket.getData()));
+                // byte[] buffer = new byte[BUFSIZE];
+                // DatagramPacket recievePacket = new DatagramPacket(buffer, buffer.length);
+                // aSocket.receive(recievePacket);
+                // System.out.println("Reply:" + new String(recievePacket.getData()));
 
-           } 
-
-           // long receiveEnd = System.currentTimeMillis();
-           // long difference = receiveEnd - receiveStart;
-           // long throughput = count / difference;
-           // System.out.println("Throughput: " + throughput);
+            }
 
         } catch (Exception e) {
             System.out.println("Socket: " + e.getMessage());
--- SEMESTER5/CO324/Lab08/udp/UDPServer.java
+++ SEMESTER5/CO324/Lab08/udp/UDPServer.java
@@ -3,10 +3,12 @@
 import java.net.*;
 import java.io.*;
 import java.util.*;
+import java.lang.*;
 
 public class UDPServer {
-    final static int PORT = 1200;
+    final static int PORT = 1234;
     final static int BUFSIZE = 1024;
+    final static int PCKTS = 100;
     
     
     public static void main(String args[]) {
@@ -14,20 +16,30 @@ public class UDPServer {
         int count = 0;
         long receiveStart = 0;
 
-        // while (!Thread.interrupted())
-        for (int i=0; i<100; i++) {
+        while (!Thread.interrupted()){
+        // for (int i=0; i<= PCKTS; i++) {
         
+    		// System.out.println("server in");
             try (DatagramSocket aSocket = new DatagramSocket(PORT);) {
+    			System.out.println("try in");
                 byte[] bufferRecieve = new byte[BUFSIZE];          
                 DatagramPacket recievePacket = new DatagramPacket(bufferRecieve, BUFSIZE);
     			
-                if (i==0) {
+                if (count==0) {
                     
+    			System.out.println("i = 0");
                     receiveStart = System.currentTimeMillis();
                 }
 
     			aSocket.receive(recievePacket);
+    			System.out.println("Packet received!");
                 count++;
+
+                if (count == PCKTS){
+                	long receiveEnd = System.currentTimeMillis();
+        			System.out.println("Throughput: " + (count * 1000/(receiveStart - receiveEnd)));
+        			count = 0;
+                }
                 
     			// DatagramPacket replyPacket = new DatagramPacket(
        //                  recievePacket.getData(), recievePacket.getLength(), 
@@ -39,9 +51,8 @@ public class UDPServer {
             } catch (Exception e) {            
                 System.out.println("Socket: " + e.getMessage());            
             } 
-        }
+        // }
 
-        long receiveEnd = System.currentTimeMillis();
-        System.out.println("Throughput: " + (count * 1000/(receiveStart - receiveEnd)));
+        }
     }
 }

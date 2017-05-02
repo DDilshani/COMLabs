7990f2fea54fbd1c37249a0c3fb399c46d72df97
udareechk <udareeck@gmail.com>
2017-05-02 13:57:06 +0530

    Tries Lab modified

--- SEMESTER5/CO322/Lab06/AutoCompleteImpl.c
+++ SEMESTER5/CO322/Lab06/AutoCompleteImpl.c
@@ -1,17 +1,22 @@
 #include "AutoCompleteImpl.h"
 
-struct TrieNode *createTrieNode() {
+TrieNode *createTrieNode() {
     //TODO implement logic for creating an Trie node
     TrieNode *node = (TrieNode *)malloc(sizeof(TrieNode));
 
-    node -> children = NULL;
-    node -> label = null;
-    isEndOfWord = false;
+    int j;
+    for (j = 0; j < ALPHABET_SIZE; j++){
+        node -> children[j] = NULL;
+    }
+
+    // node -> children = NULL;
+    node -> label = 0;
+    node -> isEndOfWord = false;
 
     return node;
 }
 
-void insert(struct TrieNode *root, const char *word) {
+void insert(TrieNode *root, const char *word) {
     //TODO implement logic for inserting a word to the tree
 
     int i;
@@ -19,45 +24,51 @@ void insert(struct TrieNode *root, const char *word) {
 
     for (i = 0; i < ARRAY_SIZE(word); i++){
 
-        int index = CHAR_TO_INDEX[word[i]];
+        int index = CHAR_TO_INDEX(word[i]);
 
-		if (relRoot -> children == NULL){
+		if (relRoot -> children[index] == NULL){
 
-			relRoot -> children = (TrieNode *)(sizeof(TrieNode) * ALPHABET_SIZE);
-            
-            int j;
-            for (j = 0; j < ALPHABET_SIZE; j++){
-                relRoot -> children[j] = NULL;
-            }
-
-			TrieNode * child = relRoot -> children[index];
+			// relRoot -> children = (TrieNode *)(sizeof(TrieNode) * ALPHABET_SIZE);
+			TrieNode * child = (TrieNode *) relRoot -> children[index];
             child = createTrieNode();
 			child -> label = word[i];
-            relRoot = child;
-            
-        } else {
+            // relRoot = child;
 
-            TrieNode * child = relRoot -> children[index]; 
-            if(!child){
-                child = createTrieNode();
-            }
+        } 
 
-            child -> label = word[i];
-            relRoot = child;
+        relRoot = (TrieNode*) relRoot -> children[index];
+           
+  //       } else {
 
-		}
+  //           TrieNode * child = relRoot -> children[index]; 
+  //           if(!child){
+  //               child = createTrieNode();
+  //           }
+
+  //           child -> label = word[i];
+  //           relRoot = child;
+
+		// }
     	
     }
 
     relRoot -> isEndOfWord = true;
 }
 
-struct TrieNode *search(struct TrieNode *root, const char *word) {
+TrieNode *search(TrieNode *root, const char *word) {
     //TODO implement search logic for Tries tree.
     //TODO This function should return last node of the node sequence where we found given word
+    int i;
+    
+    for (i = 0; i < ARRAY_SIZE(word); i++){
+
+    }
+
+
+    return NULL;
 }
 
-void traverse(char prefix[], struct TrieNode *root) {
+void traverse(char prefix[], TrieNode *root) {
     //TODO implement tree traversal logic here. Use this to traverse underneath tree
     //TODO TIP: use this function to print words once you find the node in search function
 }
--- SEMESTER5/CO322/Lab06/AutoCompleteImpl.h
+++ SEMESTER5/CO322/Lab06/AutoCompleteImpl.h
@@ -7,15 +7,15 @@
 #define ARRAY_SIZE(a) sizeof(a)/sizeof(a[0])
 #define CHAR_TO_INDEX(c) ((int)c - (int)'a')
 
-typedef struct TrieNode{
-    // struct TrieNode *children[ALPHABET_SIZE];
-    struct TrieNode *children;
+typedef struct trienode{
+    struct TrieNode *children[ALPHABET_SIZE];
+    // struct TrieNode *children;
     char label;
     bool isEndOfWord;
 } TrieNode;
 
 
-struct TrieNode *createNode();
-struct TrieNode *search(struct TrieNode *root, const char *word);
-void traverse(char prefix[], struct TrieNode *root);
-void insert(struct TrieNode *root, const char *word);
+TrieNode *createNode();
+TrieNode *search(TrieNode *root, const char *word);
+void traverse(char prefix[], TrieNode *root);
+void insert(TrieNode *root, const char *word);

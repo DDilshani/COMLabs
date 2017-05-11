df2321d6c1778185f6a44d1c7d8eafb72be02cbf
udareechk <udareeck@gmail.com>
2017-05-11 14:14:53 +0530

    Modified

--- SEMESTER5/CO322/Lab06/Part2/AutoCompleteImpl.c
+++ SEMESTER5/CO322/Lab06/Part2/AutoCompleteImpl.c
@@ -105,7 +105,8 @@ void insert(TrieNode *root, char *word) {
 
             char *labelRest = getSubString(child -> label, bp, strlen(child -> label) - bp);
             TrieNode *nextChild = createLabeledTrieNode(labelRest);
-            nextChild -> children[] = child -> children[];
+            memcpy(nextChild -> children, child -> children, sizeof(TrieNode*)*ALPHABET_SIZE);
+            // nextChild -> children[] = child -> children[];
             nextChild -> isEndOfWord = child -> isEndOfWord;
             int nextChildIndex = CHAR_TO_INDEX(labelRest[0]);
 

866fa99a3f315c69b4b3566daca4c295610525eb
udareechk <udareeck@gmail.com>
2017-05-14 15:33:46 +0530

    Time calculations finished

--- SEMESTER5/CO322/Lab06/Part1/AutoComplete.c
+++ SEMESTER5/CO322/Lab06/Part1/AutoComplete.c
@@ -1,4 +1,5 @@
 #include "AutoCompleteImpl.h"
+#include <time.h>
 
 #define NUMBER_OF_WORDS (354935)
 #define INPUT_WORD_SIZE (100)
@@ -28,12 +29,22 @@ int main() {
     // printf("%p\n",(void*)root );
     root -> label = '*';	//Marking the root node
     int i;
+
+    //To get time to insert
+    clock_t start = clock();
+
     for (i = 0; i < NUMBER_OF_WORDS; i++){
     	char *word = words[i];
     	int length = strcspn(word, "\r\n");	// get the length of the string eliminating CR and LF
     	word[length] = 0; // trim the word length by inseting null character
         insert(root, word);
     }
+
+    clock_t end = clock();
+
+    double insertionTime = (double)(end - start) / CLOCKS_PER_SEC;
+
+    printf("Time to insert: %lf\n", insertionTime);
 
     while (1) {
         printf("Enter keyword: ");
@@ -43,9 +54,19 @@ int main() {
         printf("\n********************* Possible Words ********************\n");
 
         //TODO traverse the tree and provide possible word list
+
+        //Time calculation for traverse
+        start = clock();
+
         TrieNode *subRoot = search(root, str); // get the end of entered word
         traverse(str, subRoot, strlen(str));	// Traverse the sub tree
 
         printf("==========================================================\n");
+
+        end = clock();
+
+        double traversalTime = (double)(end - start) / CLOCKS_PER_SEC;
+
+        printf("Time to traverse: %lf\n", traversalTime);
     }
 }
